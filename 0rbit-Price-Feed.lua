2ee06fe136f6f3d074f29ec2a7da39990fad67f1
ümit güneş <98491064+koumitg@users.noreply.github.com>
2024-05-19 01:19:35 +0300

    Add files via upload

--- 0rbit-Price-Feed.lua
+++ 0rbit-Price-Feed.lua
@@ -15,7 +15,7 @@ function handleError(msg, errorMessage)
     })
 end
 
-Handlers.add("https://github.com/koumitg/0rbit",
+Handlers.add("https://github.com/koumitg/",
     Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
     function(msg)
         local token = msg.Tags.Token
